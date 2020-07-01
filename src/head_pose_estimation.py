1aec81f8f5711696b5a5f3b3e30769aa13368bff
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2020-07-01 19:36:33 +0200

    Head pose estimation inference working

--- src/head_pose_estimation.py
+++ src/head_pose_estimation.py
@@ -2,6 +2,8 @@
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
+import cv2
+from openvino.inference_engine import IENetwork, IECore
 
 
 class Model_Head_Pose_Estimation:
@@ -13,7 +15,9 @@ class Model_Head_Pose_Estimation:
         '''
         TODO: Use this to set your instance variables.
         '''
-        raise NotImplementedError
+        self.model_name = model_name
+        self.device = device
+        self.cpu_extensions = extensions
 
     def load_model(self):
         '''
@@ -21,28 +25,73 @@ class Model_Head_Pose_Estimation:
         This method is for loading the model to the device specified by the user.
         If your model requires any Plugins, this is where you can load them.
         '''
-        raise NotImplementedError
+        model_xml = self.model_name + ".xml"
+        model_bin = self.model_name + ".bin"
+
+        # Inference Engine initialization
+        self.plugin = IECore()
+
+        # Read the IR as a IENetwork
+        self.network = IENetwork(model=model_xml, weights=model_bin)
+
+        supported_layers = self.plugin.query_network(
+            network=self.network, device_name="CPU")
+
+        unsupported_layers = [
+            l for l in self.network.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            print("Unsupported layers found: {}".format(unsupported_layers))
+            print("Check whether extensions are available to add to IECore.")
+            exit(1)
+
+        # Add extension if it has been specified and we are executing in that device
+        if self.cpu_extensions and "CPU" in self.device:
+            self.plugin.add_extension(self.cpu_extensions, self.device)
+
+        # Load the IENetwork (model) into the plugin
+        self.exec_network = self.plugin.load_network(self.network, self.device)
+
+        # Get the input layer
+        self.input_blob = next(iter(self.network.inputs))
+
+        return self.plugin
 
     def predict(self, image):
         '''
         TODO: You will need to complete this method.
         This method is meant for running predictions on the input image.
         '''
-        raise NotImplementedError
+        p_image = self.preprocess_input(image)
+
+        self.infer_request = self.exec_network.start_async(
+            request_id=0, inputs={self.input_blob: p_image})
+        return
 
     def check_model(self):
-        raise NotImplementedError
+        if self.infer_request.wait() == 0:
+            result = self.infer_request.outputs
+            return result
 
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
-    raise NotImplementedError
+        '''
+        Before feeding the data into the model for inference,
+        you might have to preprocess it. This function is where you can do that.
+        '''
+        net_input_shape = self.exec_network.inputs[self.input_blob].shape
+        p_frame = cv2.resize(image, (net_input_shape[3], net_input_shape[2]))
+        p_frame = p_frame.transpose((2, 0, 1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
 
     def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-    raise NotImplementedError
+        '''
+        Before feeding the output of this model to the next model,
+        you might have to preprocess the output. This function is where you can do that.
+
+        Output layer names in Inference Engine format:
+
+        name: "angle_y_fc", shape: [1, 1] - Estimated yaw (in degrees).
+        name: "angle_p_fc", shape: [1, 1] - Estimated pitch (in degrees).
+        name: "angle_r_fc", shape: [1, 1] - Estimated roll (in degrees).
+        '''
+        raise NotImplementedError
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from input_feeder import InputFeeder
 from face_detection import Model_Face_Detection
 from facial_landmarks_detection import Model_Facial_LandMarks_Detection
+from head_pose_estimation import Model_Head_Pose_Estimation
 
 
 def build_argparser():
@@ -13,9 +14,11 @@ def build_argparser():
     """
     parser = ArgumentParser()
     parser.add_argument("-fdm", "--face_detection", required=True, type=str,
-                        help="Path to the face detection model.")
+                        help="Path to face detection model.")
     parser.add_argument("-flm", "--landmark_detection_model", required=True,
-                        type=str, help="Path to the landmark detection model.")
+                        type=str, help="Path to landmark detection model.")
+    parser.add_argument("-hpm", "--head_pose_model", required=True,
+                        type=str, help="Path to head pose estimation model.")
     parser.add_argument("-it", "--input_type", required=True, type=str,
                         help="Input type that is goint to be used \'video\',"
                         " \'image\' or \'cam\'.")
@@ -36,9 +39,11 @@ def infer_on_stream(args):
     face_detection = Model_Face_Detection(args.face_detection)
     landmark_model = Model_Facial_LandMarks_Detection(
         args.landmark_detection_model)
+    head_pose_model = Model_Head_Pose_Estimation(args.head_pose_model)
 
     face_detection.load_model()
     landmark_model.load_model()
+    head_pose_model.load_model()
 
     frame = next(input_feeder.next_batch())
     face_detection.predict(frame)
@@ -51,9 +56,14 @@ def infer_on_stream(args):
     for face_box in face_boxes:
         face = frame[face_box[1]:face_box[3], face_box[0]:face_box[2]]
         landmark_model.predict(face)
+        head_pose_model.predict(face)
+
         landmarks_result = landmark_model.check_model()
         eyes_frame, left_eye, right_eye = landmark_model.preprocess_output(
             landmarks_result, face, args.debug)
+
+        head_pose_result = head_pose_model.check_model()
+        print(head_pose_result)
 
 
 def main():
